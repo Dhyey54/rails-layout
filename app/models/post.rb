ff5f68741da05c723d8feec5574faed51092dcb7
Dhyey Sapara <dhyey@simformsolutions.com>
2023-05-18 17:55:42 +0530

    Fix: Broadcast issue fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,4 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, :description, presence: true
-
-  after_create_commit -> { broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self }, target: "posts" }
 end
